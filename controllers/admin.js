e232a7d57b59e3cb190f7d1be14278dc0c9e8aba
ping58972 <ping58972@gmail.com>
2019-06-08 18:12:40 -0500

    setting authorization of user

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,22 +68,25 @@ exports.postEditProduct = (req, res, next) => {
     const updatedDesc = req.body.description;
  
        // const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl,  prodId);
-       Product.findById(prodId).then(product => {
+       Product.findById(prodId)
+       .then(product => {
+           if(product.userId.toString() !== req.user._id.toString()){
+               return res.redirect('/')
+           }
            product.title = updatedTitle;
            product.price = updatedPrice;
            product.description = updatedDesc;
            product.imageUrl = updatedImageUrl;
-           return product.save();
-       })
-        .then(result=>{
+           return product.save().then(result=>{
             console.log('UPDATE PRODUCT');
             res.redirect('/admin/products');
+       });
         }).catch(err=>console.log(err));
     //}).catch(err=>console.log(err));
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({userId: req.user._id})
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(products => {
@@ -99,7 +102,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    //Product.findByIdAndRemove(prodId)
+    Product.deleteOne({_id: prodId, userId: req.user._id})
     .then(()=>{
           console.log('DESTROYED PRODUCT');
           res.redirect('/admin/products');
